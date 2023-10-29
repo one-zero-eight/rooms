45f39d7322326fe209576d5f1e599efdab3b3fa8
Maxim Fomin <m.fomin@innopolis.university>
2023-10-29 14:41:02 +0300

    style: pre-commit changes
    For some reason pre-commit didn't work earlier. Now it does.

--- src/config.py
+++ src/config.py
@@ -2,5 +2,4 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-
-    model_config = SettingsConfigDict(env_file='.env')
+    model_config = SettingsConfigDict(env_file=".env")
--- src/main.py
+++ src/main.py
@@ -3,5 +3,5 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
