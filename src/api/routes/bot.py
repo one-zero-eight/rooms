7dccc9fb2cc6e883e57c21d10197ee903bf980ab
Maxim Fomin <m.fomin@innopolis.university>
2023-12-20 00:22:59 +0300

    feat: add a method for creating rooms

--- src/api/routes/bot.py
+++ src/api/routes/bot.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from starlette import status
 
 from src.api.auth.utils import BOT_ACCESS_DEPENDENCY
-from src.api.utils import check_room_exists, check_user_not_exists
+from src.api.utils import check_room_exists, check_user_exists, check_user_not_exists
 from src.db_sessions import DB_SESSION_DEPENDENCY
-from src.models.sql import User
-from src.schemas.db_schemas import UserSchema
-from src.schemas.method_input_schemas import CreateUserBody
+from src.models.sql import User, Room
+from src.schemas.db_schemas import UserSchema, RoomSchema
+from src.schemas.method_input_schemas import CreateUserBody, CreateRoomBody
 
 bot_router = APIRouter(prefix="/bot", dependencies=[BOT_ACCESS_DEPENDENCY])
 
@@ -25,3 +26,17 @@ async def create_user(user: CreateUserBody, db: DB_SESSION_DEPENDENCY):
         await db.commit()
 
     return new_user
+
+
+@bot_router.post("/room/create", response_model=RoomSchema)
+async def create_room(body: CreateRoomBody, db: DB_SESSION_DEPENDENCY):
+    user = await check_user_exists(body.user_id, db)
+    if user.room is not None:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, "The user already has a room")
+
+    room = Room(name=body.name)
+    user.room = room
+    db.add(room)
+    await db.commit()
+
+    return room
