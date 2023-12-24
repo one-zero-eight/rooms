8f00b249dbcd3069ef918f9a4349367350fe6683
Maxim Fomin <m.fomin@innopolis.university>
2023-12-25 01:11:23 +0300

    feat: remove ability to use register method for entering to a room

--- src/api/routes/bot.py
+++ src/api/routes/bot.py
@@ -4,7 +4,7 @@ from sqlalchemy.sql.functions import count
 from starlette import status
 
 from src.api.auth.utils import BOT_ACCESS_DEPENDENCY
-from src.api.utils import check_room_exists, check_user_not_exists, USER_DEPENDENCY, ROOM_DEPENDENCY, check_user_exists
+from src.api.utils import check_user_not_exists, USER_DEPENDENCY, ROOM_DEPENDENCY, check_user_exists
 from src.config import SETTINGS_DEPENDENCY
 from src.db_sessions import DB_SESSION_DEPENDENCY
 from src.models.sql import User, Room, Invitation, TaskExecutor, Order
@@ -23,16 +23,10 @@ bot_router = APIRouter(prefix="/bot", dependencies=[BOT_ACCESS_DEPENDENCY])
 @bot_router.post("/user/create", response_model=UserSchema)
 async def create_user(user: CreateUserBody, db: DB_SESSION_DEPENDENCY):
     await check_user_not_exists(user.user_id, db)
-    if user.room_id is not None:
-        await check_room_exists(user.room_id, db)
 
     new_user = User(user.user_id)
     db.add(new_user)
     await db.commit()
-
-    if user.room_id is not None:
-        new_user.room_id = user.room_id
-        await db.commit()
 
     return new_user
 
--- src/schemas/method_input_schemas.py
+++ src/schemas/method_input_schemas.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel, Field
 
 class CreateUserBody(BaseModel):
     user_id: int = Field(ge=0)
-    room_id: int | None = None
 
 
 class CreateRoomBody(BaseModel):
