520f06e896edabf5c556c4421f9a6eba6e5e4c0d
Maxim Fomin <m.fomin@innopolis.university>
2023-12-30 00:41:36 +0300

    fix: remove duplicating `details` field in error responses

--- src/api/exception_handlers.py
+++ src/api/exception_handlers.py
@@ -1,8 +1,9 @@
-from fastapi import HTTPException
 from fastapi.responses import JSONResponse
+
+from src.api.exceptions import MyException
 
 
 # noinspection PyUnusedLocal
-async def error_printing(request, exception: HTTPException):
+async def error_printing(request, exception: MyException):
     print(exception)
-    return JSONResponse({"detail": exception.detail}, exception.status_code, exception.headers)
+    return JSONResponse(exception.detail, exception.status_code, exception.headers)
