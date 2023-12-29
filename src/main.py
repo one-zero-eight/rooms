dc86d41c87ddc504585b996df4337f2d6e030deb
Maxim Fomin <m.fomin@innopolis.university>
2023-12-30 00:18:21 +0300

    feat: add redirect from / to /docs

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI, HTTPException
+from starlette.responses import RedirectResponse
 
 from src.api.routes.bot import bot_router
 from src.api.exception_handlers import error_printing
@@ -9,6 +10,12 @@ app = FastAPI()
 app.add_exception_handler(HTTPException, error_printing)
 
 app.include_router(bot_router)
+
+
+@app.get("/")
+def docs_redirect():
+    return RedirectResponse("/docs")
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
