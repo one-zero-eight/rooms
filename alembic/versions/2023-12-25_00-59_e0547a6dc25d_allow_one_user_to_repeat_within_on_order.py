fb6cb83efba88664e176c9d38bdfe7d7bf155c72
Maxim Fomin <fominmaxim3721@gmail.com>
2024-05-30 01:33:55 +0300

    chore: fix migration to correctly downgrade repeating executors

--- alembic/versions/2023-12-25_00-59_e0547a6dc25d_allow_one_user_to_repeat_within_on_order.py
+++ alembic/versions/2023-12-25_00-59_e0547a6dc25d_allow_one_user_to_repeat_within_on_order.py
@@ -9,7 +9,8 @@ Create Date: 2023-12-25 00:47:29.406628
 from typing import Sequence, Union
 
 from alembic import op
-
+from sqlalchemy import delete, table, select, func, exists, column
+from sqlalchemy.orm import Session
 
 # revision identifiers, used by Alembic.
 revision: str = "e0547a6dc25d"
@@ -25,6 +26,19 @@ def upgrade() -> None:
 
 
 def downgrade() -> None:
+    with Session(bind=op.get_bind()) as db:
+        orders_table = table("orders", column("id"))
+        db.execute(
+            delete(orders_table).where(
+                exists(
+                    select(column("user_id"))
+                    .select_from(table("executors"))
+                    .where(column("order_id") == orders_table.c.id)
+                    .group_by(column("user_id"))
+                    .having(func.count() >= 2)
+                )
+            )
+        )
     op.alter_column("executors", "order_number", autoincrement=False)
     op.drop_constraint("executors_pkey", "executors", type_="primary")
     op.create_primary_key("executors_pkey", "executors", ["user_id", "order_id"])
